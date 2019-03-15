407cd9a54f0c282509a167c8a10bccea981ebc50
Pat Graczyk <Pat.Graczyk@sainsburysbank.co.uk>
2019-03-15 11:47:44 +0000

    revert

--- src/main/java/db/DBTest.java
+++ src/main/java/db/DBTest.java
@@ -10,5 +10,23 @@ import org.hibernate.criterion.Restrictions;
 public class DBTest {
     private static Session session;
 
+    public static List<Product> findAllProductsOfThis(Basket basket){
+        session = HibernateUtil.getSessionFactory().openSession();
+        List<Product> result = null;
+        try{
+
+            Criteria cr = session.createCriteria(Product.class);
+            cr.add(Restrictions.eq("id", basket.getId()));
+            result =  cr.list();
+//            cr.addOrder(Order.desc("id"));
+        }
+        catch (HibernateException ex){
+            ex.printStackTrace();
+        }finally {
+            session.close();
+        }
+
+        return result;
+
     }
 }
